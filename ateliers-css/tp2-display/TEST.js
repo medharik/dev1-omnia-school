f88d927f5086e0fca9964c2a6df4168e7797ac1b
medharik <med.harik@gmail.com>
2024-12-01 15:06:07 +0100

    display inline block avec valign et vh

---
+++ ateliers-css/tp2-display/TEST.js
@@ -0,0 +1,9 @@
+let age=10;
+let majeur= age ?? 
+if (age>18) {
+   
+    console.log('majeur')
+} else {
+    
+    console.log('mineur')
+}
---
+++ ateliers-css/tp2-display/test2.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @param {Number} a number to raise
+ * @param {Number} b  un nombre 
+ * @returns {Number}  la somme a retourner 
+ */
+
+
+const t=(a,b)=>a+b;
+let x=0,y=10;
+
+console.log('test1',t(1,2));
+console.log('test2',t(10,2));
+x=prompt('test ?',0);
+// console.log(x);
+
+
+
